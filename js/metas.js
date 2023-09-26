27bf2a48aa0477f194d502e46fafa06270af2404
Breno Tosi <breno_tosi@hotmail.com>
2023-09-26 19:02:47 -0300

    ajustes e inicio post de feedback

--- js/metas.js
+++ js/metas.js
@@ -1,102 +1,180 @@
 var metas = [
-    { "group": "Social", "color": "red", "text": "Melhorar saúde e nutrição do produto", "value": "Improve product health & nutrition by achieving 85% compliance with Kraft Heinz Global Nutrition Targets by 2025." },
-    { "group": "Social", "color": "red", "text": "Reduzir açúcar total em produtos", "value": "Reduce total sugar in our products by more than 60 million pounds across our global portfolio by 2025." },
-    { "group": "Social", "color": "red", "text": "Reduzir sódio em molhos e molhos para salada", "value": "Reduce sodium by an additional 5% in our BBQ Sauce and Kraft Salad Dressings in North America by 2025." },
-    { "group": "Social", "color": "red", "text": "Melhorar ingredientes simples e transparência", "value": "Improve use and transparency of simpler ingredients by 2025." },
-    { "group": "Social", "color": "red", "text": "Aumentar produtos à base de plantas", "value": "Increase our plant-based offerings." },
-    { "group": "Social", "color": "red", "text": "Fornecer 1,5 bilhão de refeições para necessitados", "value": "Provide 1.5 billion meals to people in need by 2025 against our 2019 baseline." },
-    { "group": "Social", "color": "red", "text": "50% de posições de gestão global ocupadas por mulheres", "value": "50% of our global management positions will be filled by women by 2025." },
-    { "group": "Social", "color": "red", "text": "30% de funcionários nos EUA serão de minorias étnicas", "value": "30% of our salaried U.S. employee population will identify as people of color by 2025." },
-    { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Reduzir intensidade uso de água em áreas de risco.", 
-        "value": "Reduce water use intensity by 20% in high-risk watershed areas by 2025 (per metric ton of product made)."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Reduzir intensidade uso de água em instalações.", 
-        "value": "Reduce water use intensity by 15% across our manufacturing facilities by 2025 (per metric ton of product made)."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Reduzir intensidade resíduos em instalações.", 
-        "value": "Reduce waste to landfill intensity by 20% across our manufacturing facilities by 2025 (per metric ton of product made)."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Embalagens 100% recicláveis, reutilizáveis ou compostáveis.", 
-        "value": "Aim to make 100% recyclable, reusable or compostable packaging by 2025."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Garrafa Heinz Tomato Ketchup totalmente circular na Europa.", 
-        "value": "Create a fully circular Heinz Tomato Ketchup Bottle in Europe by 2022."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Alcançar emissões líquidas zero até 2050, reduzindo pela metade até 2030.", 
-        "value": "Achieve Net Zero carbon emissions by 2050, halving same by 2030."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Reduzir intensidade uso de energia em instalações.", 
-        "value": "Reduce energy use intensity by 15% across our manufacturing facilities by 2025 (per metric ton of product made)."
-      },
-      { 
-        "group": "Ambiental", 
-        "color": "green",
-        "text": "Obter a maioria da eletricidade de fontes renováveis até 2025.", 
-        "value": "Procure majority of electricity from renewable sources by 2025."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Fonte global 100% ovos de galinhas criadas sem gaiolas até 2025.", 
-        "value": "Source 100% of eggs globally from cage-free or better* hens by 2025."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Fonte de ovos na Europa de galinhas ao ar livre.", 
-        "value": "Source 100% of eggs in Europe from free-range hens."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Compra 100% tomates sustentáveis para Heinz Ketchup até 2025.", 
-        "value": "Purchase 100% sustainably-sourced Heinz Ketchup tomatoes by 2025."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Compra de 100% de óleo de palma sustentável até 2022.", 
-        "value": "Purchase 100% sustainable palm oil by 2022."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Compra de óleo de palma rastreável até a usina.", 
-        "value": "Purchase 100% traceable palm oil to the mill by 2022."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Melhorar bem-estar de frangos nos EUA até 2024.", 
-        "value": "Improve broiler chicken welfare in the U.S. by 2024 and European chicken commitment by 2026."
-      },
-      { 
-        "group": "Governança", 
-        "color": "Orange",
-        "text": "Trabalhar com especialistas em bem-estar animal e fornecedores.", 
-        "value": "Continue to work with animal welfare experts and suppliers on best practices to eliminate painful procedures and promote sustainable practices."
-      }
+  {
+    "group": "Ambiental",
+    "color": "green",
+    "text": "Reduzir a intensidade do uso de energia em nossas instalações em 15% até 2025 (por tonelada métrica de produto fabricado).",
+    "value": "9f516b7b-6b7d-491b-b790-8430877185b2",
+    "range": 50.0
+  },
+  {
+    "group": "Governança",
+    "color": "orange",
+    "text": "Fonte de 100% dos ovos globalmente de galinhas criadas sem gaiolas ou em condições melhores até 2025.",
+    "value": "924b46d6-f673-40ad-bc88-df54ff92b716",
+    "range": 50.0
+  },
+  {
+    "group": "Governança",
+    "color": "orange",
+    "text": "Compra de 100% de óleo de palma sustentável até 2022.",
+    "value": "cac7d0d6-fa9e-485c-9c6a-3d9c54d6c14f",
+    "range": 50.0
+  },
+  {
+    "group": "Governança",
+    "color": "orange",
+    "text": "Compra de 100% de óleo de palma rastreável até a usina até 2022.",
+    "value": "c14dc9a3-b5d0-4f37-9c09-68c5e919aa81",
+    "range": 50.0
+  },
+  {
+    "group": "Social",
+    "color": "red",
+    "text": "Melhorar a saúde e nutrição dos produtos, alcançando 85% de conformidade com as Metas Globais de Nutrição da Kraft Heinz até 2025.",
+    "value": "704f26b5-2031-44ca-8321-4cd12a40750d",
+    "range": 50.0
+  },
+  {
+    "group": "Social",
+    "color": "red",
+    "text": "Reduzir o açúcar total em nossos produtos em mais de 60 milhões de libras em nosso portfólio global até 2025.",
+    "value": "6795e293-30ee-4753-98bf-c00df553fdd1",
+    "range": 50.0
+  },
+  {
+    "group": "Social",
+    "color": "red",
+    "text": "Reduzir o sódio em mais 5% em nosso Molho de Churrasco e Molhos para Salada Kraft na América do Norte até 2025.",
+    "value": "f408fa4a-e6f7-46e9-b049-f6e3f24acfbc",
+    "range": 50.0
+  },
+  {
+    "group": "Social",
+    "color": "red",
+    "text": "Aumentar nossas ofertas de produtos à base de plantas.",
+    "value": "c4dbe6fd-b455-4f51-be6f-ad6bd0556784",
+    "range": 50.0
+  }
+,
+{
+  "group": "Social",
+  "color": "red",
+  "text": "Fornecer 1,5 bilhão de refeições para pessoas necessitadas até 2025, em relação à nossa base de 2019.",
+  "value": "ff7f311d-85be-4fca-94ba-9b4ebcfc0aef",
+  "range": 50.0
+}
+,
+{
+  "group": "Social",
+  "color": "red",
+  "text": "50% de nossas posições de gestão global serão ocupadas por mulheres até 2025.",
+  "value": "61590428-cbe0-4308-a7c2-baa408a1a0e8",
+  "range": 50.0
+}
+,
+{
+  "group": "Social",
+  "color": "red",
+  "text": "30% de nossa população de funcionários assalariados nos EUA se identificará como pessoas de cor até 2025.",
+  "value": "4bd30e48-a8b2-4e5f-8756-09704c9dbafd",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Reduzir a intensidade do uso de água em nossas instalações em 15% até 2025 (por tonelada métrica de produto fabricado).",
+  "value": "e483c06d-3bbc-4a81-8b83-d4ba06335d2e",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Visamos criar embalagens 100% recicláveis, reutilizáveis ou compostáveis até 2025.",
+  "value": "32e80e10-e848-487b-9f8f-5863b6145dd2",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Criar uma garrafa Heinz Tomato Ketchup totalmente circular na Europa até 2022.",
+  "value": "47eb2b21-4cab-43e0-9b52-ddf3a65498d3",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Alcançar emissões líquidas zero de carbono até 2050, reduzindo pela metade até 2030.",
+  "value": "a547339d-a501-47a1-a230-9cedd316b726",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Buscar a obtenção da maioria da eletricidade de fontes renováveis até 2025.",
+  "value": "bf3ce5cb-ace2-42c9-a0e7-897664ea79bc",
+  "range": 50.0
+}
+,
+{
+  "group": "Governança",
+  "color": "orange",
+  "text": "Fonte de 100% dos ovos na Europa de galinhas criadas ao ar livre.",
+  "value": "6da0beee-9aad-4055-bc9b-63f5800e298d",
+  "range": 50.0
+}
+,
+{
+  "group": "Governança",
+  "color": "orange",
+  "text": "Compra de 100% dos tomates sustentáveis para o Heinz Ketchup até 2025.",
+  "value": "7eb57f2c-a286-48d9-bf4c-69b40877e9fd",
+  "range": 50.0
+}
+,
+{
+  "group": "Governança",
+  "color": "orange",
+  "text": "Melhorar o bem-estar de frangos de corte nos EUA até 2024 e o compromisso europeu com frangos até 2026.",
+  "value": "77250b24-11ae-4943-a2a8-44954d8191b3",
+  "range": 50.0
+}
+,
+{
+  "group": "Social",
+  "color": "red",
+  "text": "Melhorar o uso e a transparência de ingredientes mais simples até 2025.",
+  "value": "99de6356-f18f-42d0-b886-f872768c9a8f",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Reduzir a intensidade do uso de água em áreas de risco em 20% até 2025 (por tonelada métrica de produto fabricado).",
+  "value": "f4927a61-92d6-4d4c-8fe7-c84c30ca19fa",
+  "range": 50.0
+}
+,
+{
+  "group": "Ambiental",
+  "color": "green",
+  "text": "Reduzir a intensidade de resíduos enviados para aterro sanitário em nossas instalações em 20% até 2025 (por tonelada métrica de produto fabricado).",
+  "value": "f316096e-c89f-41f9-acab-7afe0a5bece7",
+  "range": 50.0
+}
+,
+{
+  "group": "Governança",
+  "color": "orange",
+  "text": "Continuar a trabalhar com especialistas em bem-estar animal e fornecedores nas melhores práticas para eliminar procedimentos dolorosos e promover práticas sustentáveis.",
+  "value": "17027066-a4c7-4d46-8034-8e420902a06d",
+  "range": 50.0
+}
 ];
 
 const metaSocial = document.getElementById('social');
@@ -110,8 +188,6 @@ const metasGovernantes = metas.filter(meta => meta.group === "Governança")
 console.log(metasAmbientais);
 
 metasSociais.forEach(meta => {
-    const randomPercentage = Math.round(Math.random(100)*100);
-    const randomPercentageText = randomPercentage.toString();
     const card = document.createElement('div');
     card.classList.add('card');
   
@@ -140,12 +216,12 @@ metasSociais.forEach(meta => {
     progress.min = 0;
     progress.step = 1;
     progress.max = 100;
-    progress.value = randomPercentage;
+    progress.value = meta.range;
     progress.classList.add('progress-red');
   
     const progressText = document.createElement('p');
     progressText.classList.add('bold', 'color-black');
-    progressText.textContent = randomPercentageText + '%';
+    progressText.textContent = meta.range + '%';
   
     progressBar.appendChild(progress);
     progressBar.appendChild(progressText);
@@ -158,7 +234,7 @@ metasSociais.forEach(meta => {
   
     card.appendChild(cardLink);
   
-    metaSocial.appendChild(card);
+    metaSocial.appendChild(card);;
   });
 
 metasAmbientais.forEach(meta => {
@@ -192,12 +268,12 @@ metasAmbientais.forEach(meta => {
     progress.min = 0;
     progress.step = 1;
     progress.max = 100;
-    progress.value = randomPercentageText;
+    progress.value = meta.range;
     progress.classList.add('progress-green');
   
     const progressText = document.createElement('p');
     progressText.classList.add('bold', 'color-black');
-    progressText.textContent = randomPercentageText + '%';
+    progressText.textContent = meta.range + '%';
   
     progressBar.appendChild(progress);
     progressBar.appendChild(progressText);
@@ -245,12 +321,12 @@ metasGovernantes.forEach(meta => {
     progress.min = 0;
     progress.step = 1;
     progress.max = 100;
-    progress.value = randomPercentage;
+    progress.value = meta.range;
     progress.classList.add('progress-yellow');
   
     const progressText = document.createElement('p');
     progressText.classList.add('bold', 'color-black');
-    progressText.textContent = randomPercentageText + '%';
+    progressText.textContent = meta.range + '%';
   
     progressBar.appendChild(progress);
     progressBar.appendChild(progressText);
